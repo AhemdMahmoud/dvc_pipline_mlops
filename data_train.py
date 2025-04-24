e4f67fdb4e75e13a1622edad23094d1b02f4853d
Ahmed Mahmoud <ahmedmamoud148@gmail.com>
2025-04-25 02:38:33 +0300

    lg_added

--- data_train.py
+++ data_train.py
@@ -73,7 +73,7 @@ def train_model(X_train, y_train, plot_name='', class_weight=None):
 
     global clf_name, all_metrics
 
-    clf = LogisticRegression(c=2.5, max_iter=1000, random_state=45, class_weight=class_weight)
+    clf = LogisticRegression(C=2.5, max_iter=10000, random_state=45, class_weight=class_weight)
     clf.fit(X_train, y_train)
 
     y_pred_test = clf.predict(X_test_final)
